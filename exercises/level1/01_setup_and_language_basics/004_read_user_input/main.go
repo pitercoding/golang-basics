64ecfceb766420e7f8810931318d83b5c8e83177
pitercoding <piterg.bio@gmail.com>
2026-05-18 16:54:03 +0200

    feat(exercises): add level 1 read user input exercise (EX-1.01.004)

---
+++ exercises/level1/01_setup_and_language_basics/004_read_user_input/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	var age int
+
+	fmt.Print("Enter your name: ")
+	fmt.Scanln(&name)
+
+	fmt.Print("Enter your age: ")
+	fmt.Scanln(&age)
+
+	fmt.Printf("\nHi, %s! You are %d years old.\n", name, age)
+}
