d74b69a7cbd724a8dfecde027233d61ba7ace6ad
pitercoding <piterg.bio@gmail.com>
2026-05-19 16:35:15 +0200

    feat(exercises): add level 1 printf formatting exercise (EX-1.01.010)

---
+++ exercises/level1/01_setup_and_language_basics/010_printf_formatting/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Racha Cuca"
+	age := 49
+	weight := 68.8
+
+	fmt.Printf("Name: %s\n", name)
+	fmt.Printf("Age: %d\n", age)
+	fmt.Printf("Weight: %.1f kg\n", weight)
+}
